474e0ec9809d92ea27e18ae60b0347be05152a00
mindtechindustriesai-alt <mindtech.industriesai@gmail.com>
2026-08-26 16:51:49 +0200

    Add docstring and export service modules
    Added module docstring and updated exports for services.

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,1 +1,20 @@
+"""
+SalaryPlan Services Package
+Exports all service modules for AI, knowledge, language, and quantum functionality.
+"""
 
+from .deepseek_service import DeepSeekService, get_deepseek_service
+from .knowledge_base import KnowledgeBase, get_knowledge_base
+from .language_service import LanguageService, get_language_service
+from .quantum_service import QuantumService, get_quantum_service
+
+__all__ = [
+    "DeepSeekService",
+    "get_deepseek_service",
+    "KnowledgeBase",
+    "get_knowledge_base",
+    "LanguageService",
+    "get_language_service",
+    "QuantumService",
+    "get_quantum_service"
+]
